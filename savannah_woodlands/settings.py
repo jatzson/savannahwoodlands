9c83f543d7b8bc5189606bbff455535ddd344ddc
precious <kelechiokere10@gmail.com>
2026-07-13 23:48:43 +0100

    update ALLOWED_HOSTS for custom domain

--- savannah_woodlands/settings.py
+++ savannah_woodlands/settings.py
@@ -66,3 +66,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = [
+    "savannahwoodlands.com",
+    "www.savannahwoodlands.com",
+    ".onrender.com",
+]
